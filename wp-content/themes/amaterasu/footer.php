6430d67a338b64faa22ae3221b4dbfdd4f95d0f3
0xAckerMan <joelkores1@gmail.com>
2023-05-24 19:02:04 +0300

    initial commit

---
+++ wp-content/themes/amaterasu/footer.php
@@ -0,0 +1,6 @@
+<h4>This is the footer</h4>
+
+
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ wp-content/themes/amaterasu/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php wp_head(); ?>
+</head>
+<body>
+
+<header>
+    <div class="header-left">
+        Joel Kores
+    </div>
+    <div class="header-right">
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About Me</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </div>
+</header>
+    
