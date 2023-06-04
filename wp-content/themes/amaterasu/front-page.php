5769bdd9ed5d8f33d754b89e50465b965289600f
0xAckerMan <joelkores1@gmail.com>
2023-06-04 21:29:36 +0300

    Ui checking

--- wp-content/themes/amaterasu/front-page.php
+++ wp-content/themes/amaterasu/front-page.php
@@ -2,10 +2,88 @@
 
 
 <div class="container">
-    <h1><?php the_title(); ?></h1>
+      <div class="left-container">
+        <div class="info">
+          <h1>Joel Kores</h1>
+          <h2>Software and Security Engineer</h2>
+          <p>
+            I build and secure, inclusive and accessible <br />
+            digital softwares for the world
+          </p>
+        </div>
 
-    <?php get_template_part('includes/section', 'content'); ?>
+        <div class="navigation">
+          <ul class="nav">
+            <li><a href="#" class="active">ABOUT</a></li>
+            <li><a href="#">EXPERIENCE</a></li>
+            <li><a href="#">PROJECTS</a></li>
+          </ul>
+        </div>
 
+        <div class="socials">
+          <ul class="midia">
+            <li>
+              <a href="https://www.github.com/0xAckerMan" target="_blank">
+                <span class="fab fa-github-square" aria-hidden="true"></span>
+                <span class="sr-only">Github</span>
+              </a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/joelkores/" target="_blank">
+                <span class="fab fa-linkedin" aria-hidden="true"></span>
+                <span class="sr-only">LinkedIn</span>
+              </a>
+            </li>
+
+            <li>
+              <a href="#" target="_blank">
+                <span class="fab fa-medium" aria-hidden="true"></span>
+                <span class="sr-only">Medium</span>
+              </a>
+            </li>
+            <li>
+              <a href="#" target="_blank">
+                <span class="fab fa-twitter" aria-hidden="true"></span>
+                <span class="sr-only">Medium</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="right-container">
+        <section id="aboutme">
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia blanditiis numquam aspernatur nemo, qui fugit obcaecati veritatis enim ullam! Fugiat ratione dolores nobis vel pariatur dolorum tenetur officiis, minima ab.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, a! Ab fugit alias aperiam esse, ipsam ducimus. Qui autem tenetur aliquam animi hic ex facilis dolorem, asperiores quae vitae! Quis!
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus atque minima sint, natus deserunt laboriosam a obcaecati nobis suscipit debitis facere dolore corporis similique aut ratione eius dolor optio dolorum?</p>
+        </section>
+
+
+
+
+        <div class="colors">
+          <div class="navy"></div>
+          <div class="lightnavy"></div>
+          <div class="lightestnavy"></div>
+          <div class="slate"></div>
+          <div class="lightslate"></div>
+          <div class="lightestslate"></div>
+          <div class="white"></div>
+          <div class="green"></div>
+        </div>
+
+      </div>
+    </div>
+    <script
+      type="module"
+      src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"
+    ></script>
+    <script
+      nomodule
+      src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"
+    ></script>
+
+    <script src="script.js"></script>
 
 
 <?php get_footer(); ?>
