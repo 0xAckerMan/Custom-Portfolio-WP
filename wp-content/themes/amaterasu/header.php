e94892671a5f5c048f0c86c64ef0ef91f0eeaadd
0xAckerMan <joelkores1@gmail.com>
2023-06-14 09:49:36 +0300

    Nav bar in place

--- wp-content/themes/amaterasu/header.php
+++ wp-content/themes/amaterasu/header.php
@@ -5,6 +5,59 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans">
     <?php wp_head(); ?>
 </head>
 <body>
+<div class="container">
+      <div class="left-container">
+        <div class="upper">
+        <div class="info">
+          <h1>Joel Kores</h1>
+          <h2>Software and Security Engineer</h2>
+          <p>
+            I build and secure, inclusive and accessible <br />
+            digital softwares for the world
+          </p>
+        </div>
+
+        <div class="navigation">
+          <ul class="nav">
+            <li><a href="#aboutme" class="active">ABOUT</a></li>
+            <li><a href="#experience">EXPERIENCE</a></li>
+            <li><a href="#">PROJECTS</a></li>
+          </ul>
+        </div>
+        </div>
+
+        <div class="socials">
+          <ul class="midia">
+            <li>
+              <a href="https://www.github.com/0xAckerMan" target="_blank">
+                <span class="fab fa-github-square" aria-hidden="true"></span>
+                <span class="sr-only">Github</span>
+              </a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/joelkores/" target="_blank">
+                <span class="fab fa-linkedin" aria-hidden="true"></span>
+                <span class="sr-only">LinkedIn</span>
+              </a>
+            </li>
+
+            <li>
+              <a href="#" target="_blank">
+                <span class="fab fa-medium" aria-hidden="true"></span>
+                <span class="sr-only">Medium</span>
+              </a>
+            </li>
+            <li>
+              <a href="#" target="_blank">
+                <span class="fab fa-twitter" aria-hidden="true"></span>
+                <span class="sr-only">Medium</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
