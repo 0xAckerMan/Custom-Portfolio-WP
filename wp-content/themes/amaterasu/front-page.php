e4d530a0680c23ac121ceda53c6bd71afb7306cb
0xAckerMan <joelkores1@gmail.com>
2023-06-14 09:48:55 +0300

    the experience section

--- wp-content/themes/amaterasu/front-page.php
+++ wp-content/themes/amaterasu/front-page.php
@@ -1,61 +1,106 @@
 <?php get_header(); ?>
 
-
-<div class="container">
-      <div class="left-container">
-        <div class="info">
-          <h1>Joel Kores</h1>
-          <h2>Software and Security Engineer</h2>
-          <p>
-            I build and secure, inclusive and accessible <br />
-            digital softwares for the world
-          </p>
-        </div>
-
-        <div class="navigation">
-          <ul class="nav">
-            <li><a href="#" class="active">ABOUT</a></li>
-            <li><a href="#">EXPERIENCE</a></li>
-            <li><a href="#">PROJECTS</a></li>
-          </ul>
-        </div>
-
-        <div class="socials">
-          <ul class="midia">
-            <li>
-              <a href="https://www.github.com/0xAckerMan" target="_blank">
-                <span class="fab fa-github-square" aria-hidden="true"></span>
-                <span class="sr-only">Github</span>
-              </a>
-            </li>
-            <li>
-              <a href="https://www.linkedin.com/in/joelkores/" target="_blank">
-                <span class="fab fa-linkedin" aria-hidden="true"></span>
-                <span class="sr-only">LinkedIn</span>
-              </a>
-            </li>
-
-            <li>
-              <a href="#" target="_blank">
-                <span class="fab fa-medium" aria-hidden="true"></span>
-                <span class="sr-only">Medium</span>
-              </a>
-            </li>
-            <li>
-              <a href="#" target="_blank">
-                <span class="fab fa-twitter" aria-hidden="true"></span>
-                <span class="sr-only">Medium</span>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-
       <div class="right-container">
         <section id="aboutme">
           <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia blanditiis numquam aspernatur nemo, qui fugit obcaecati veritatis enim ullam! Fugiat ratione dolores nobis vel pariatur dolorum tenetur officiis, minima ab.
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, a! Ab fugit alias aperiam esse, ipsam ducimus. Qui autem tenetur aliquam animi hic ex facilis dolorem, asperiores quae vitae! Quis!
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus atque minima sint, natus deserunt laboriosam a obcaecati nobis suscipit debitis facere dolore corporis similique aut ratione eius dolor optio dolorum?</p>
+        </section>
+
+        <section id="experience">
+          <div class="work-card">
+            <div class="years">
+              <p>2019 - 2021</p>
+            </div>
+            <div class="work-info">
+              <div class="work-upper">
+                <div class="job-title">
+                  <h3>Software Engineer</h3>
+                </div>
+                <div class="company">
+                  <h4>The Jitu</h4>
+                </div>
+              </div>
+              <div class="job-description">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia blanditiis numquam aspernatur nemo, qui fugit obcaecati veritatis enim ullam! Fugiat ratione dolores nobis vel pariatur dolorum tenetur officiis, minima ab.
+                </p>
+              </div>
+              <div class="job-tech">
+                <ul>
+                  <li>PHP</li>
+                  <li>JavaScript</li>
+                  <li>React</li>
+                  <li>Node</li>
+                  <li>Python</li>
+                  <li>Java</li>
+                </ul>
+
+            </div>
+          </div>
+          </div>
+
+          <div class="work-card">
+            <div class="years">
+              <p>2019 - 2021</p>
+            </div>
+            <div class="work-info">
+              <div class="work-upper">
+                <div class="job-title">
+                  <h3>Software Engineer</h3>
+                </div>
+                <div class="company">
+                  <h4>The Jitu</h4>
+                </div>
+              </div>
+              <div class="job-description">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia blanditiis numquam aspernatur nemo, qui fugit obcaecati veritatis enim ullam! Fugiat ratione dolores nobis vel pariatur dolorum tenetur officiis, minima ab.
+                </p>
+              </div>
+              <div class="job-tech">
+                <ul>
+                  <li>PHP</li>
+                  <li>JavaScript</li>
+                  <li>React</li>
+                  <li>Node</li>
+                  <li>Python</li>
+                  <li>Java</li>
+                </ul>
+
+            </div>
+          </div>
+          </div>
+
+
+          <div class="work-card">
+            <div class="years">
+              <p>2019 - 2021</p>
+            </div>
+            <div class="work-info">
+              <div class="work-upper">
+                <div class="job-title">
+                  <h3>Software Engineer</h3>
+                </div>
+                <div class="company">
+                  <h4>The Jitu</h4>
+                </div>
+              </div>
+              <div class="job-description">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia blanditiis numquam aspernatur nemo, qui fugit obcaecati veritatis enim ullam! Fugiat ratione dolores nobis vel pariatur dolorum tenetur officiis, minima ab.
+                </p>
+              </div>
+              <div class="job-tech">
+                <ul>
+                  <li>PHP</li>
+                  <li>JavaScript</li>
+                  <li>React</li>
+                  <li>Node</li>
+                  <li>Python</li>
+                  <li>Java</li>
+                </ul>
+
+            </div>
+            </div>
+          </div>
         </section>
 
 
@@ -83,7 +128,6 @@
       src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"
     ></script>
 
-    <script src="script.js"></script>
 
 
 <?php get_footer(); ?>
