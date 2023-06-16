2453d27f055a05deeab7e0e23665cefb1d81d3d2
0xAckerMan <joelkores1@gmail.com>
2023-06-16 10:24:44 +0300

    few changes

--- wp-content/themes/amaterasu/front-page.php
+++ wp-content/themes/amaterasu/front-page.php
@@ -2,37 +2,38 @@
 
       <div class="right-container">
         <section id="aboutme">
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia blanditiis numquam aspernatur nemo, qui fugit obcaecati veritatis enim ullam! Fugiat ratione dolores nobis vel pariatur dolorum tenetur officiis, minima ab.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, a! Ab fugit alias aperiam esse, ipsam ducimus. Qui autem tenetur aliquam animi hic ex facilis dolorem, asperiores quae vitae! Quis!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus atque minima sint, natus deserunt laboriosam a obcaecati nobis suscipit debitis facere dolore corporis similique aut ratione eius dolor optio dolorum?</p>
+          <p>Welcome to my portfolio! I'm Joel Kores, a passionate security and software engineer with a knack for crafting innovative solutions. With a creative mindset and a strong dedication to excellence, I strive to deliver projects that push the boundaries of what's possible.</p>
+          <p>My love for technology ignited in high school when I first laid eyes on a computer. Since then, I have embarked on an exciting journey of continuous learning and skill acquisition, spanning from cyber security to software engineering. This diverse skill set allows me to approach projects with a holistic perspective, ensuring both robust security and seamless functionality.</p>
+          <p>My journey is driven by a genuine passion for technology and its potential to make a positive impact. I approach every project with enthusiasm and a commitment to excellence, aiming to exceed expectations and contribute to the success of organizations.</p>
+          <p>Thank you for visiting my portfolio. Feel free to explore my projects and get in touch to discuss how I can bring my expertise to your next venture. Together, we can shape the future through innovative and secure technology solutions.</p>
         </section>
 
         <section id="experience">
         <div class="work-card">
               <div class="years">
-                <p>2021 - 2023</p>
+                <p>2023 APR - 2023</p>
               </div>
               <div class="work-info">
                 <div class="work-upper">
                   <div class="job-title">
-                    <h3>Software Engineer - The Jitu</h3>
+                    <h3>Software Engineer Trainee - The Jitu</h3>
                   </div>
                   <!-- <div class="company">
                     <h4>The Jitu</h4>
                   </div> -->
                 </div>
                 <div class="job-description">
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia blanditiis numquam aspernatur nemo, qui fugit obcaecati veritatis enim ullam! Fugiat ratione dolores nobis vel pariatur dolorum tenetur officiis, minima ab.
+                  <p>Software Engineering trainee focused on wordpress development. Learned how to build custom themes and custom plugins and make my site scallable</p>
                   </p>
                 </div>
                 <div class="job-tech">
                   <ul>
+                    <li>Wordpress</li>
                     <li>PHP</li>
-                    <li>JavaScript</li>
-                    <li>React</li>
-                    <li>Node</li>
-                    <li>Python</li>
-                    <li>Java</li>
+                    <li>HTML</li>
+                    <li>css</li>
+                    <li>thems</li>
+                    <li>plugins</li>
                   </ul>
 
               </div>
@@ -42,16 +43,16 @@
 
           <div class="work-card">
             <div class="years">
-              <p>2021 - 2023</p>
+              <p>2022 - 2023</p>
             </div>
             <div class="work-info">
               <div class="work-upper">
                 <div class="job-title">
-                  <h3>Software Engineer</h3>
+                  <h3>Chief Technology Officer- The Startup</h3>
                 </div>
-                <div class="company">
+                <!-- <div class="company">
                   <h4>The Jitu</h4>
-                </div>
+                </div> -->
               </div>
               <div class="job-description">
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia blanditiis numquam aspernatur nemo, qui fugit obcaecati veritatis enim ullam! Fugiat ratione dolores nobis vel pariatur dolorum tenetur officiis, minima ab.
@@ -75,30 +76,30 @@
 
         <section id="projects" >
         <div class="work-card">
-              <div class="years">
-                <p>2021 - 2023</p>
+              <div class="proj-pic">
+                <img src="https://i.imgur.com/29r387L.png" alt="plana">
               </div>
               <div class="work-info">
                 <div class="work-upper">
                   <div class="job-title">
-                    <h3>Software Engineer - The Jitu</h3>
+                    <h3>Event Management System</h3>
                   </div>
                   <!-- <div class="company">
                     <h4>The Jitu</h4>
                   </div> -->
                 </div>
                 <div class="job-description">
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia blanditiis numquam aspernatur nemo, qui fugit obcaecati veritatis enim ullam! Fugiat ratione dolores nobis vel pariatur dolorum tenetur officiis, minima ab.
+                <p>Plana, a event management system that enables event organizers to manage their events and attendees to register for events. The system is built with PHP, JavaScript, WOrdpress, Custom themes and custom plugins</p>
                   </p>
                 </div>
                 <div class="job-tech">
                   <ul>
+                    <li>Wordpress</li>
                     <li>PHP</li>
-                    <li>JavaScript</li>
-                    <li>React</li>
-                    <li>Node</li>
-                    <li>Python</li>
-                    <li>Java</li>
+                    <li>HTML</li>
+                    <li>css</li>
+                    <li>thems</li>
+                    <li>plugins</li>
                   </ul>
 
               </div>
@@ -106,9 +107,18 @@
           </div>
         </section>
 
+        <section id="contact">
+          <p>Get in Touch</p>
+          <div class="contact">
+            <div class="contact-form">
+              <?php echo do_shortcode('[contact-form-7 id="17" title="Contact form 1"]');?>
+            </div>
+          </div>
+        </section>
 
 
 
+<!-- 
         <div class="colors">
           <div class="navy"></div>
           <div class="lightnavy"></div>
@@ -118,7 +128,7 @@
           <div class="lightestslate"></div>
           <div class="white"></div>
           <div class="green"></div>
-        </div>
+        </div> -->
 
       </div>
     </div>
@@ -131,7 +141,6 @@
       src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"
     ></script>
 
-    <!-- echo do_shortcode('[contact-form-7 id="17" title="Contact form 1"]'); -->
 
 
 <?php get_footer(); ?>
